b7970d8262596270860286bfb1f53aa1ed0a54fa
Anandamaya <arnoananda@gmail.com>
2020-09-05 17:08:09 +0200

    Updates

---
+++ precache-manifest.1fcc2eba0aacc4987ecd3b796b5aae0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f9dccec49fa8bec6ac5da99ac4f7af",
+    "url": "/react-app-test/index.html"
+  },
+  {
+    "revision": "06852bc0babb894aee2c",
+    "url": "/react-app-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "48d11cb059d9cd057037",
+    "url": "/react-app-test/static/js/2.b7442e6d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-test/static/js/2.b7442e6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06852bc0babb894aee2c",
+    "url": "/react-app-test/static/js/main.f866e821.chunk.js"
+  },
+  {
+    "revision": "d2da184b647e6553ae16",
+    "url": "/react-app-test/static/js/runtime-main.5160438b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
